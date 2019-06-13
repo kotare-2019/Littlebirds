f535f0bdea6d284fa08f4acf0904d3758145de20
Kandace Willcox <kiwdev1984@icloud.com>
2019-06-13 18:14:40 +1200

    yeet

--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,16 @@ const db = require('../db')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  db.getUsers()
-    .then(users => {
-      res.render('index', {users: users})
-    })
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
-})
+// router.get('/', (req, res) => {
+//   db.getUsers()
+//     .then(users => {
+//       res.render('index', {users: users})
+//     })
+//     .catch(err => {
+//       res.status(500).send('DATABASE ERROR: ' + err.message)
+//     })
+// })
+
+
 
 module.exports = router
