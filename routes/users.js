11df2cc69917ee7f10899bad48e8e6e7bf944d2d
student <tom.doesburg@gmail.com>
2019-06-13 17:02:16 +1200

    initial commit

--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,18 @@ const db = require('../db')
 
 const router = express.Router()
 
+// router.get('/', (req, res) => {
+//   db.getUsers()
+//     .then(users => {
+//       res.render('index', {users: users})
+//     })
+//     .catch(err => {
+//       res.status(500).send('DATABASE ERROR: ' + err.message)
+//     })
+// })
+
 router.get('/', (req, res) => {
-  db.getUsers()
-    .then(users => {
-      res.render('index', {users: users})
-    })
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
+  res.render('main')
 })
 
 module.exports = router
