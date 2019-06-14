91ef28dbccaa379ccd76e1b65760e4c818f4a045
student <tom.doesburg@gmail.com>
2019-06-14 15:25:10 +1200

    fgdrv

--- knexfile.js
+++ knexfile.js
@@ -24,11 +24,10 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
+    // database: 'my_db',
+    // user: 'username',
+    // password: 'password'
     pool: {
       min: 2,
       max: 10
